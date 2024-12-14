0fa9eaae5845345633d301e3083e4baa9a6a3b59
mohsenkord <mohsen3kord77@gmail.com>
2024-12-14 14:12:05 +0330

    Adding posts with fake data by faker

--- core/accounts/tests/test_user_accounts.py
+++ core/accounts/tests/test_user_accounts.py
@@ -7,3 +7,11 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 
+@pytest.mark.django_db
+class TestAccountUser:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(email='testuser@gmail.com', password='testpassword')
+        self.client.force_authenticate(user=self.user)
+
